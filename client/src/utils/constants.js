6ad1ac1ca206578c7aab387dd45b652ce69dc16b
Ammar Ansari <iammaransari@gmail.com>
2022-07-04 00:58:58 +0500

    heroku api production url update

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,6 @@
 export const BOARD_ROWS = 6;
 export const BOARD_COLS = 6;
 export const API_BASE_PATH =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://upcomingurl.com/api';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://board-game-atompoint.herokuapp.com/api';
